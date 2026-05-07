c0f91115c78c16c3354605e08541ffb90ad37c2e
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-05-07 20:40:50 +0300

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int right = digits.size() - 1;
+
+        for(int i = right ; i >= 0 ; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+
+        digits.insert(digits.begin() , 1);
+        return digits;
+    }
+};
