57a4ae6f27c2021cb10a25ba4942cd077155c497
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-06-05 16:54:01 +0300

    Add: single-element-in-a-sorted-array - submission-1

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+       int left = 0;
+       int right = nums.size() - 1;
+
+       while(left < right)
+       {
+        int mid = left + (right - left) / 2;
+
+        if(mid %2  == 1)
+            mid--;
+        
+        if(nums[mid] != nums[mid + 1])
+        {
+            // go left 
+            right = mid;
+        }
+        else
+        {
+            // go right
+            left += 2;
+        }
+
+
+       }
+
+       return nums[left];
+    }
+};
