5ee267cb3f1c80cf4aaf4a5bd4028acd2fb45941
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-06-05 19:18:27 +0300

    Add: isomorphic-strings - submission-3

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char , char> mp;
+        unordered_map<char , char> used;
+
+        for(int i = 0 ; i < s.size(); i++)
+        {
+            char a = s[i];
+            char b = t[i];
+
+            if(mp.count(a))
+            {
+                if(mp[a] != b)
+                    return false;
+            }
+            else
+            {
+                if(used.count(b))
+                    return false;
+                
+                mp[a] = b;
+                used[b] = a;
+            }
+           
+        }
+
+        return true;
+    }
+};
